ff38f02a92df0a49a183ce6dfbfc72592ad2a1cb
Erivis5643 <erik.visinka0@gmail.com>
2025-08-11 00:01:33 +0200

    Delete Halal-Rating directory

--- Halal-Rating/config.example.js
+++
@@ -1,7 +0,0 @@
-// Kopiere diese Datei zu config.js und trage deine Supabase Keys ein
-window.SUPABASE_CONFIG = {
-  url: "https://ssteasnvniqsgowkgkne.supabase.co",
-  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNzdGVhc252bmlxc2dvd2tna25lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ4NTMzMDgsImV4cCI6MjA3MDQyOTMwOH0.OZ8_32NAfZ_qoXo_shUd8PatTyhVvpcBr4o-AnSQnw4"
-};
-
-
